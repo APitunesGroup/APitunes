f10cc79d04d100d62d054d1ee58057b6f86061fa
jonathandavidblack <jonnybl915@gmail.com>
2016-07-08 15:28:36 -0400

    added login route

--- src/main/java/com/theironyard/controllers/APitunesRestController.java
+++ src/main/java/com/theironyard/controllers/APitunesRestController.java
@@ -1,10 +1,17 @@
 package com.theironyard.controllers;
 
+import com.theironyard.entities.User;
 import com.theironyard.services.LikeRepository;
 import com.theironyard.services.SongRepository;
 import com.theironyard.services.UserRepository;
+import com.theironyard.utilities.PasswordStorage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by jonathandavidblack on 7/8/16.
@@ -20,5 +27,22 @@ public class APitunesRestController {
 
     @Autowired
     LikeRepository likes;
+
+
+    @RequestMapping(path = "/login", method = RequestMethod.POST)
+    public User login(@RequestBody User user, HttpSession session) throws Exception {
+        User userFromDatabase = users.findFirstByUsername(user.getUsername());
+        if (userFromDatabase == null) {
+            user.setPassword(PasswordStorage.createHash(user.getPassword()));
+            user.setUsername(user.getUsername());
+            users.save(user);
+        }
+        else if (!PasswordStorage.verifyPassword(user.getPassword(), userFromDatabase.getPassword())) {
+            throw new Exception("BAD PASS");
+        }
+        session.setAttribute("username", user.getUsername());
+        return user;
+    }
+
 
 }
