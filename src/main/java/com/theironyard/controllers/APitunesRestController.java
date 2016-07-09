203a2b660d65c80700b287583e26a60c545a0882
jonathandavidblack <jonnybl915@gmail.com>
2016-07-09 15:19:02 -0400

    Merge branch 'master' of https://github.com/APitunesGroup/APitunes
    # Conflicts:
    #	src/main/java/com/theironyard/controllers/APitunesRestController.java

--- src/main/java/com/theironyard/controllers/APitunesRestController.java
+++ src/main/java/com/theironyard/controllers/APitunesRestController.java
@@ -7,10 +7,8 @@ import com.theironyard.services.SongRepository;
 import com.theironyard.services.UserRepository;
 import com.theironyard.utilities.PasswordStorage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
