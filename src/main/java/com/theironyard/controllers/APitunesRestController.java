fb45ec63738610ec83b3551fb1773d0b1820e499
Erik <eschneider1128@gmail.com>
2016-07-09 15:13:32 -0400

    added upload route

--- src/main/java/com/theironyard/controllers/APitunesRestController.java
+++ src/main/java/com/theironyard/controllers/APitunesRestController.java
@@ -1,5 +1,4 @@
 package com.theironyard.controllers;
-import static java.util.Comparator.comparing;
 
 import com.theironyard.entities.Song;
 import com.theironyard.entities.User;
@@ -12,10 +11,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -63,6 +65,32 @@ public class APitunesRestController {
         //test here...
         // sout sorted songs
         return sortedSongs;
+    }
+
+    @RequestMapping(path = "/upload", method = RequestMethod.POST)
+    public void addSong (MultipartFile audioFile, HttpSession session, String artist, String title, String genre, HttpServletResponse response) throws Exception {
+        String username = (String) session.getAttribute("username");
+        User user = users.findFirstByUsername(username);
+        if (user.isArtist() == false) {
+            throw new Exception("Must be an artist to upload.");
+        }
+
+        if (audioFile.getContentType().contains("audio")) {
+            File dir = new File("Public/songs");
+            dir.mkdirs();
+            File songFile = File.createTempFile("song", audioFile.getOriginalFilename(), dir);
+            FileOutputStream fos = new FileOutputStream(songFile);
+            fos.write(audioFile.getBytes());
+            Song song = new Song(songFile.getName(), artist, title, genre, user);
+            songs.save(song);
+
+        }
+        else {
+            throw new Exception("Invalid File Type");
+        }
+
+        response.sendRedirect("/#/artist");
+
     }
 }
 
