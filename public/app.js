9a646164d55569f2023d5f0835887b880400ce0d
Riggan Hart <rigganhart@Riggans-MacBook.local>
2016-07-10 14:53:01 -0400

    removed alert box

--- public/app.js
+++ public/app.js
@@ -9,7 +9,7 @@ module.exports = function(app) {
 
 
 
-      getArtistSongs();
+      songService.getArtistSongs();
 
 
 
@@ -140,7 +140,7 @@ module.exports = function(app){
 module.exports = function(app){
 
 // this service will handle all user data
-  app.factory('userService', ['$http', function($http){
+  app.factory('userService', ['$http','$location', function($http, $location){
 
 
 
@@ -156,10 +156,10 @@ module.exports = function(app){
                 password: pass,
               }
           }).then(function(response) {
-            console.log("got response", response);
+            console.log("got response", response.data.artist);
 
-            if(response.data.artist === true){
-              console.log("got response", response);
+            if(response.data.artist === false){
+              // console.log("got response", response);
               $location.path('/artist');
             } else {
               alert("you need to be a user to access this page. Please use the guest link")
