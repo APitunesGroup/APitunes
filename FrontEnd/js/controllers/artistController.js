463095391a3d2863c84ea8251b0beb34962fa7b3
Riggan Hart <rigganhart@Riggans-MacBook.local>
2016-07-10 18:01:49 -0400

    working on putting username on page

--- FrontEnd/js/controllers/artistController.js
+++ FrontEnd/js/controllers/artistController.js
@@ -1,14 +1,17 @@
 module.exports = function(app) {
     app.controller('artistController', ['$scope', 'userService', 'songService', function($scope, userService, songService) {
       $scope.artistSongList = songService.artistSongList;
-      $scope.artistSongList = songService.allSongList;
+      // $scope.artistSongList = songService.allSongList;
+      $scope.user = userService.currentUser;
 
 
+
+      userService.getCurrentUser();
       console.log("user info", userService.currentUser);
 
       // upload button click event
         songService.getArtistSongs();
-      
+
 
     }]);
 };
--- public/app.js
+++ public/app.js
@@ -2,14 +2,17 @@
 module.exports = function(app) {
     app.controller('artistController', ['$scope', 'userService', 'songService', function($scope, userService, songService) {
       $scope.artistSongList = songService.artistSongList;
-      $scope.artistSongList = songService.allSongList;
+      // $scope.artistSongList = songService.allSongList;
+      $scope.user = userService.currentUser;
 
 
+
+      userService.getCurrentUser();
       console.log("user info", userService.currentUser);
 
       // upload button click event
         songService.getArtistSongs();
-      
+
 
     }]);
 };
@@ -175,8 +178,13 @@ module.exports = function(app){
               console.log(currentUser);
             }
           })
-          return currentUser;
       },
+
+      getCurrentUser: function() {
+        console.log("user info", currentUser);
+        return currentUser
+      },
+
 
     };
   }]);
